34f9481dcb7f0605dcbf853dabe0497653e5dc8e
Aditya Alok <alok@termux.dev>
2025-02-07 12:50:58 +0530

    test: Stripped and newer packaging
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/ghc-cross/build.sh
+++ packages/ghc-cross/build.sh
@@ -9,38 +9,87 @@ TERMUX_PKG_DEPENDS="libiconv, libffi, libgmp, libandroid-posix-semaphore"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --host=$TERMUX_BUILD_TUPLE
---with-system-libffi"
+--with-system-libffi
+--disable-ld-override"
 TERMUX_PKG_BLACKLISTED_ARCH="i686"
 TERMUX_PKG_NO_STATICSPLIT=true
 
-termux_step_post_get_source() {
-  termux_setup_ghc && termux_setup_cabal
-  cabal update
-}
-
 termux_step_pre_configure() {
-  export CONF_CC_OPTS_STAGE1="$CFLAGS $CPPFLAGS" CONF_GCC_LINKER_OPTS_STAGE1="$LDFLAGS"
-  export CONF_CC_OPTS_STAGE2="$CFLAGS $CPPFLAGS" CONF_GCC_LINKER_OPTS_STAGE2="$LDFLAGS"
+  # WARNING: Do not move it below exported (CONF_*) options.
+  # They will get included in `settings` file of host `ghc` during `termux_setup_ghc`.
+  # NOTE: This has been fixed in `termux_setup_ghc`. Remove when changes are upstreamed.
+  termux_setup_ghc && termux_setup_cabal
 
-  export flavour="perf"
+  export CONF_CC_OPTS_STAGE1="$CFLAGS $CPPFLAGS"
+  export CONF_GCC_LINKER_OPTS_STAGE1="$LDFLAGS"
+  export CONF_CXX_OPTS_STAGE1="$CXXFLAGS"
 
-  target="$TERMUX_HOST_PLATFORM"
+  export CONF_CC_OPTS_STAGE2="$CFLAGS $CPPFLAGS"
+  export CONF_GCC_LINKER_OPTS_STAGE2="$LDFLAGS"
+  export CONF_CXX_OPTS_STAGE2="$CXXFLAGS"
+
+  export target="$TERMUX_HOST_PLATFORM"
+  export flavour="release+split_sections"
+
   if [ "$TERMUX_ARCH" = "arm" ]; then
     target="armv7a-linux-androideabi"
-    flavour+="+no_profiled_libs" # Otherwise takes more than 6 hrs to build.
+    flavour="${flavour}+no_profiled_libs" # Otherwise, build exceeds the 6 hours limit of github CI.
   fi
-  TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --target=$target"
 
-  ./boot.source
+  TERMUX_PKG_EXTRA_CONFIGURE_ARGS="$TERMUX_PKG_EXTRA_CONFIGURE_ARGS --target=$target"
 }
 
 termux_step_make() {
   (
     unset CFLAGS CPPFLAGS LDFLAGS # For stage0 compilation.
-    ./hadrian/build binary-dist-xz --flavour="$flavour" --docs=none \
-      "stage1.*.ghc.*.opts += -optl-landroid-posix-semaphore"
+    ./hadrian/build binary-dist-dir -j"$TERMUX_PKG_MAKE_PROCESSES" --flavour="$flavour" --docs=none \
+      "stage1.ghc-bin.ghc.link.opts += -optl-landroid-posix-semaphore"
   )
+}
 
-  cp ./_build/bindist/ghc-*.tar.xz "$TAR_OUTPUT_DIR"/
-  exit 0
+termux_step_make_install() {
+  cd _build/bindist/ghc-"$TERMUX_PKG_VERSION"-"$target" || exit 1
+
+  # We need to re-run configure:
+  # See: https://gitlab.haskell.org/ghc/ghc/-/issues/22058
+  ./configure \
+    --prefix="$TERMUX_PREFIX" \
+    --with-system-libffi \
+    --disable-ld-override \
+    --host="$target"
+
+  make install
+}
+
+termux_step_install_license() {
+  install -Dm600 -t "$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME" \
+    "$TERMUX_PKG_SRCDIR/LICENSE"
+}
+
+termux_step_post_massage() {
+  # Remove cross-prefix from binaries and fix links:
+  for path in bin/"$target"-*; do
+    newpath="${path//$target-/}"
+
+    if [ -h "$path" ]; then
+      link_target="$(readlink "$path")"
+      ln -sf "${link_target//$target-/}" "$newpath"
+      rm "$path"
+    else
+      mv "$path" "$newpath"
+    fi
+
+  done
+
+  local ghclibs_dir="lib/$target-ghc-$TERMUX_PKG_VERSION"
+
+  if ! [ -d "$ghclibs_dir" ]; then
+    echo "ERROR: GHC lib directory is not at expected place. Please verify before updating."
+    exit 1
+  fi
+
+  find . -type f \( -name "*.so" -o -name "*.a" \) -exec "$STRIP" --strip-unneeded {} \;
+  find "$ghclibs_dir"/bin -type f -exec "$STRIP" {} \;
+
+  tar cvzf "$TAR_OUTPUT_DIR"/ghc-"$TERMUX_PKG_VERSION"-"$TERMUX_ARCH".tar.xz lib/ bin/ share/ && exit 0
 }
