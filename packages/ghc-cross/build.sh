4fa4d584e3405002a8c973702d080c21d43691fa
Aditya Alok <alok@termux.dev>
2025-03-01 21:01:29 +0530

    pkg(ghc-cross): Enable `iserv` build
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/ghc-cross/build.sh
+++ packages/ghc-cross/build.sh
@@ -11,9 +11,67 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --host=$TERMUX_BUILD_TUPLE
 --with-system-libffi
 --disable-ld-override"
+TERMUX_PKG_HOSTBUILD=true
+
+__build_hadrian() {
+  local version="9.10.1"
+  local cache_dir="$TERMUX_COMMON_CACHEDIR/hadrian-cache"
+  local tar_file="${cache_dir}/ghc-${version}.tar.xz"
+  local unpack_dir="${cache_dir}/ghc-${version}"
+  local ghc_install_dir="${cache_dir}/ghc-${version}-runtime"
+
+  local hadrian_install_dir="${cache_dir}/hadrian"
+
+  export PATH="${hadrian_install_dir}:$PATH"
+
+  test -d "$hadrian_install_dir" && return
+
+  mkdir -p "$cache_dir"
+  termux_download "https://downloads.haskell.org/~ghc/${version}/ghc-${version}-x86_64-ubuntu20_04-linux.tar.xz" \
+    "$tar_file" \
+    ae3be406fdb73bd2b0c22baada77a8ff2f8cde6220dd591dc24541cfe9d895eb
+
+  mkdir -p "$unpack_dir" "$ghc_install_dir"
+  tar xf "$tar_file" --strip-components=1 -C "$unpack_dir"
+
+  (
+    set -e
+    unset CC CXX CFLAGS CXXFLAGS CPPFLAGS LDFLAGS AR AS CPP LD RANLIB READELF STRIP
+
+    cd "$unpack_dir"
+    ./configure --prefix="$ghc_install_dir"
+    make install
+
+  ) &>/dev/null
+
+  rm -rf "$unpack_dir" "$tar_file"
+
+  mkdir -p "$hadrian_install_dir"
+
+  (
+    set -e
+    cd "$TERMUX_PKG_SRCDIR"
+    PATH="$ghc_install_dir/bin:$PATH" cabal --project-file="$PWD/hadrian/cabal.project" new-build \
+      --disable-documentation \
+      --disable-profiling \
+      --disable-library-profiling \
+      --constraint="hadrian -threaded" \
+      -j exe:hadrian
+
+    PATH="$ghc_install_dir/bin:$PATH" cabal list-bin --project-file="$PWD/hadrian/cabal.project" hadrian:exes |
+      xargs install -Dm755 -t "$hadrian_install_dir"
+  )
+
+  rm -rf "$ghc_install_dir"
+}
+
+termux_step_host_build() {
+  termux_setup_cabal
+  __build_hadrian
+}
 
 termux_step_pre_configure() {
-  termux_setup_ghc && termux_setup_cabal
+  termux_setup_ghc
 
   export CONF_CC_OPTS_STAGE1="$CFLAGS $CPPFLAGS"
   export CONF_GCC_LINKER_OPTS_STAGE1="$LDFLAGS"
@@ -38,7 +96,11 @@ termux_step_pre_configure() {
 termux_step_make() {
   (
     unset CFLAGS CPPFLAGS LDFLAGS # For stage0 compilation.
-    ./hadrian/build binary-dist-dir -j"$TERMUX_PKG_MAKE_PROCESSES" --flavour="$flavour" --docs=none \
+    hadrian binary-dist-dir \
+      -j"$TERMUX_PKG_MAKE_PROCESSES" \
+      --flavour="$flavour" \
+      --docs=none \
+      --directory="$PWD" \
       "stage1.unix.ghc.link.opts += -optl-landroid-posix-semaphore"
   )
 }
