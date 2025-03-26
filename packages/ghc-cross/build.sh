be597b39cc18433aa37eac0ba2660a5ec2bb15e2
Aditya Alok <alok@termux.dev>
2025-03-27 00:52:45 +0530

    bump(ghc-cross): to v9.12.2
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/ghc-cross/build.sh
+++ packages/ghc-cross/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://www.haskell.org/ghc/
 TERMUX_PKG_DESCRIPTION="The Glasgow Haskell Compiler"
 TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_MAINTAINER="Aditya Alok <alok@termux.dev>"
-TERMUX_PKG_VERSION=9.12.1
+TERMUX_PKG_VERSION=9.12.2
 TERMUX_PKG_SRCURL="https://downloads.haskell.org/~ghc/$TERMUX_PKG_VERSION/ghc-$TERMUX_PKG_VERSION-src.tar.xz"
-TERMUX_PKG_SHA256=4a7410bdeec70f75717087b8f94bf5a6598fd61b3a0e1f8501d8f10be1492754
+TERMUX_PKG_SHA256=0e49cd5dde43f348c5716e5de9a5d7a0f8d68d945dc41cf75dfdefe65084f933
 TERMUX_PKG_DEPENDS="libiconv, libffi, libgmp, libandroid-posix-semaphore"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -12,8 +12,35 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-system-libffi
 --disable-ld-override"
 
+__setup_bootstrap_compiler() {
+  local version=9.10.1
+  local temp_folder="$TERMUX_PKG_CACHEDIR/ghc-bootstrap-$version"
+  local tarball="$temp_folder.tar.xz"
+  local runtime_folder="$temp_folder-runtime"
+
+  export PATH="$runtime_folder/bin:$PATH"
+
+  [[ -d "$runtime_folder" ]] && return
+
+  termux_download "https://downloads.haskell.org/~ghc/$version/ghc-$version-x86_64-ubuntu20_04-linux.tar.xz" \
+    "$tarball" \
+    ae3be406fdb73bd2b0c22baada77a8ff2f8cde6220dd591dc24541cfe9d895eb
+
+  mkdir -p "$temp_folder" "$runtime_folder"
+  tar xf "$tarball" --strip-components=1 -C "$temp_folder"
+  (
+    set -e
+    unset CC CXX CFLAGS CXXFLAGS CPPFLAGS LDFLAGS AR AS CPP LD RANLIB READELF STRIP
+    cd "$temp_folder"
+    ./configure --prefix="$runtime_folder"
+    make install
+  ) >/dev/null
+
+  rm -Rf "$temp_folder" "$tarball"
+}
+
 termux_step_pre_configure() {
-  termux_setup_ghc && termux_setup_cabal
+  __setup_bootstrap_compiler && termux_setup_cabal
 
   export CONF_CC_OPTS_STAGE1="$CFLAGS $CPPFLAGS"
   export CONF_GCC_LINKER_OPTS_STAGE1="$LDFLAGS"
@@ -24,7 +51,12 @@ termux_step_pre_configure() {
   export CONF_CXX_OPTS_STAGE2="$CXXFLAGS"
 
   export target="$TERMUX_HOST_PLATFORM"
-  [[ "$TERMUX_ARCH" == "arm" ]] && target="armv7a-linux-androideabi"
+  export profiled_libs=""
+
+  if [[ "$TERMUX_ARCH" == "arm" ]]; then
+    target="armv7a-linux-androideabi"
+    profiled_libs="+no_profiled_libs" # NOTE: We do not build profiled libs for arm. It exceeds the 6 hours usage limit of github CI.
+  fi
 
   TERMUX_PKG_EXTRA_CONFIGURE_ARGS="$TERMUX_PKG_EXTRA_CONFIGURE_ARGS --target=$target"
   ./boot.source
@@ -36,7 +68,7 @@ termux_step_make() {
 
     ./hadrian/build binary-dist-dir \
       -j"$TERMUX_PKG_MAKE_PROCESSES" \
-      --flavour="perf+no_profiled_libs" \
+      --flavour="release+split_sections$profiled_libs" \
       --docs=none \
       "stage1.unix.ghc.link.opts += -optl-landroid-posix-semaphore" \
       "stage2.unix.ghc.link.opts += -optl-landroid-posix-semaphore"
